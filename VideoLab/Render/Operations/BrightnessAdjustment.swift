d8b7bd289c38b7ecb4a17c5dcf676d8a434d8747
wanglichao <wanglichao@laien.io>
2023-08-18 10:34:32 +0800

    add brightness animation

--- VideoLab/Render/Operations/BrightnessAdjustment.swift
+++ VideoLab/Render/Operations/BrightnessAdjustment.swift
@@ -5,6 +5,7 @@
 //  Created by Bear on 2020/8/21.
 //  Copyright © 2020 Chocolate. All rights reserved.
 //
+import CoreMedia
 
 public class BrightnessAdjustment: BasicOperation {
     public var brightness:Float = 0.0 {
@@ -17,5 +18,11 @@ public class BrightnessAdjustment: BasicOperation {
         super.init(fragmentFunctionName: "brightnessFragment", numberOfInputs: 0)
         
         ({ brightness = 0.0 })()
+    }
+    
+    public override func updateAnimationValues(at time: CMTime) {
+        if let brightness = KeyframeAnimation.value(for: "brightness", at: time, animations: animations) {
+            self.brightness = brightness
+        }
     }
 }
